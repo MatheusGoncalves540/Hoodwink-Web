22e72e4dedfe3c214421b264b65bb43c5199a981
MatheusGoncalves540 <matheussouzagoncalves540@gmail.com>
2023-05-30 14:48:28 -0300

    Cliente recebe um ClientID quando conectado

--- Server/index.js
+++ Server/index.js
@@ -2,20 +2,30 @@
 const http = require("http");
 const webSocketServer = require("websocket").server;
 const httpServer = http.createServer();
-httpServer.listen(8080, () => console.log("escutando.. 8080"))
+httpServer.listen(9090, () => console.log("escutando.. 9090"))
 
 const wsServer = new webSocketServer({
     "httpServer": httpServer
 })
 
+//requisição de pagina html
+const app = require("express")();
+path = require('path')
+app.get("/", (req,res) => res.sendFile(path.join(__dirname, '..', 'Cliente', 'index.html')))
+app.listen(9091, () => console.log("escutando.. 9091"))
+
 //mapa de clientes
 const clients = {};
 
 //função que gera um uuid v4
-function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
+function uuidv4(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
 }
 
 wsServer.on("request", request => {
