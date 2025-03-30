da5f70530381dbbdc740c57fe07aa7a900dadc6c
Matheus Gonçalves <matheussouzagoncalves540@gmail.com>
2025-03-30 13:53:06 -0300

    remoção de código inutil

--- client/src/services/registerService.ts
+++ client/src/services/registerService.ts
@@ -4,8 +4,6 @@ export class RegisterService {
   static async register(nickname: string, email: string, password: string) {
     try {
       const response = await apiServer.post("/register", { nickname, email, password });
-      const token = response.data.jwtToken;
-      document.cookie = `jwt=${token}`;
       return response.data;
     } catch (error : any) {
       return error.response.data
