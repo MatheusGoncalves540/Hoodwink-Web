32b9ad341b4f14d4fe9db52f4e36070833df68b8
Matheus Gonçalves <matheussouzagoncalves540@gmail.com>
2025-03-29 21:48:52 -0300

    demodularização e debbuger

--- server/src/hoodwinkModule.ts
+++ server/src/hoodwinkModule.ts
@@ -1,12 +1,10 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { DatabaseModule } from "./modules/databaseModule";
-import { RegisterModule } from "./modules/registerModule";
 import { UsersModule } from "./modules/usersModule";
-import { LoginModule } from "./modules/loginModule";
 import { AuthService } from "./services/authService";
 import { RefreshTokenMiddleware } from "./middleware/refreshTokenMiddleware";
 @Module({
-  imports: [DatabaseModule, UsersModule, RegisterModule, LoginModule],
+  imports: [DatabaseModule, UsersModule],
   controllers: [],
   providers: [AuthService],
 })
--- server/src/modules/usersModule.ts
+++ server/src/modules/usersModule.ts
@@ -1,12 +1,17 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { LoginController } from "src/controller/loginController";
+import { RegisterController } from "src/controller/registerController";
 import { User } from "src/database/entity/userEntity";
+import { AuthService } from "src/services/authService";
+import { LoginService } from "src/services/loginService";
+import { RegisterService } from "src/services/registerService";
 import { UsersService } from "src/services/usersService";
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
-    controllers: [],
-    providers: [UsersService],
+    controllers: [LoginController, RegisterController],
+    providers: [UsersService, RegisterService, LoginService, AuthService],
     exports:[UsersService]
 })
 export class UsersModule { }
