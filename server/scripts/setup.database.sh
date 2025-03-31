613ff3fcbd938b0d476913c80b46ace9a56084e9
Matheus Gonçalves <matheussouzagoncalves540@gmail.com>
2025-03-30 23:41:22 -0300

    script update

--- server/scripts/setup.database.sh
+++ server/scripts/setup.database.sh
@@ -1,3 +1,9 @@
+#################################################################
+## RODAR DENTRO DO TERMINAL DO WSL CASO ESTEJA NO WINDOWS!     ##
+## NÃO EXECUTE ESSE ARQUIVO, APENAS COPIE E RODE OS COMANDOS!  ##
+#################################################################
+
+mkdir ./postgres:
 podman create --name postgresdb \
   -e POSTGRES_USER=postgres \
   -e POSTGRES_PASSWORD=postgres \
@@ -5,13 +11,9 @@ podman create --name postgresdb \
   -p 5432:5432 \
   -v ./postgres:/data/db \
   docker.io/library/postgres
+podman start postgresdb
 
-podman start postgres
-#########
-
-#########
 podman network create redis-network
-
 echo 'requirepass redis
 loadmodule /opt/redis-stack/lib/redisearch.so MAXSEARCHRESULTS 10000 MAXAGGREGATERESULTS 10000
 loadmodule /opt/redis-stack/lib/rediscompat.so
@@ -19,16 +21,13 @@ loadmodule /opt/redis-stack/lib/redisbloom.so
 loadmodule /opt/redis-stack/lib/redistimeseries.so
 loadmodule /opt/redis-stack/lib/redisgears.so v8-plugin-path /opt/redis-stack/lib/libredisgears_v8_plugin.so
 loadmodule /opt/redis-stack/lib/rejson.so' > redis.cof
-
 podman create --name redisdb \
   -p 6379:6379 \
   --network redis-network \
   -v $(pwd)/redis.conf:/etc/redis.conf:ro \
   docker.io/redis/redis-stack-server:latest \
   redis-server /etc/redis.conf
-
 podman start redisdb
-
 podman run --name redis-commander \
   --network redis-network \
   -d -p 8081:8081 \
@@ -37,6 +36,6 @@ podman run --name redis-commander \
   -e REDIS_PASSWORD=redis \
   docker.io/rediscommander/redis-commander
 
-http://localhost:8081 #acessar redis-commander
+#http://localhost:8081 #acessar redis-commander
 
 # Executar migrations e seeds após essas etapas
